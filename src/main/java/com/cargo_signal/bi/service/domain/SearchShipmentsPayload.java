7a6189961cb4ad6b50a0c0001a09a3a3c347520d
jasoneglin <jason.eglin@expeditors.com>
2022-02-07 08:57:34 -0800

    fix(domain): using lombok

--- src/main/java/com/cargo_signal/bi/service/domain/SearchShipmentsPayload.java
+++ src/main/java/com/cargo_signal/bi/service/domain/SearchShipmentsPayload.java
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 @Getter
+@Setter
 @EqualsAndHashCode
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
